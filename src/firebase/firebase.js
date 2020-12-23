b9729b8a5257b441d5f1ed06959ca70ed73d07b8
Tuan Phan <tphan@seattleu.edu>
2020-12-22 20:46:04 -0800

    Finished the app

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOlB-KMrEr-enYRpVAGZKaMW3BWRGaVTk",
+  apiKey: "",
   authDomain: "clone-dbb60.firebaseapp.com",
   databaseURL: "https://clone-dbb60.firebaseio.com",
   projectId: "clone-dbb60",
   storageBucket: "clone-dbb60.appspot.com",
-  messagingSenderId: "673937686900",
-  appId: "1:673937686900:web:38584a7380a03e2dd12c21",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
